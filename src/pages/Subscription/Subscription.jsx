410e325a1678c2b9e864ab946f528a3fe2a880dd
fahim-sarker <sarkerfahim599@gmail.com>
2025-05-13 09:36:42 +0600

    add pages

--- src/pages/Subscription/Subscription.jsx
+++ src/pages/Subscription/Subscription.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <div>
+        <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas facilis illo necessitatibus sapiente quibusdam iusto, quod magnam rerum mollitia perspiciatis, eos esse quisquam, corporis ducimus ad maiores maxime quasi dignissimos natus unde officia hic modi fugit consequatur? Officia facere culpa consequuntur recusandae, beatae temporibus voluptatem cupiditate eius maiores, odit soluta.z</h2>
+    </div>
   )
 }
 
